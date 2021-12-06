7ca6aa0edbfb16d8d8a7639b3a1fdc3803a7c278
Jeremy Hunt <huntingfrogs@hotmail.com>
2021-12-06 19:08:11 +1300

    Refine Day 6

--- 06-2/src/Main.elm
+++ 06-2/src/Main.elm
@@ -10,29 +10,33 @@ solve str =
         |> List.filterMap String.toInt
         |> Dict.Extra.groupBy identity
         |> Dict.map (\_ fishes -> List.length fishes)
-        |> iterate 256
+        |> iterate 256 step
         |> Dict.values
         |> List.sum
 
 
-iterate times fishes =
+iterate times f xs =
     case times of
         0 ->
-            fishes
+            xs
 
         _ ->
-            iterate (times - 1) (step fishes)
+            iterate (times - 1) f (f xs)
 
 
 step : Dict Int Int -> Dict Int Int
 step fishes =
+    let
+        old n =
+            Dict.get n fishes |> Maybe.withDefault 0
+    in
     fishes
-        |> Dict.update 0 (always (Dict.get 1 fishes |> Maybe.withDefault 0 |> Just))
-        |> Dict.update 1 (always (Dict.get 2 fishes |> Maybe.withDefault 0 |> Just))
-        |> Dict.update 2 (always (Dict.get 3 fishes |> Maybe.withDefault 0 |> Just))
-        |> Dict.update 3 (always (Dict.get 4 fishes |> Maybe.withDefault 0 |> Just))
-        |> Dict.update 4 (always (Dict.get 5 fishes |> Maybe.withDefault 0 |> Just))
-        |> Dict.update 5 (always (Dict.get 6 fishes |> Maybe.withDefault 0 |> Just))
-        |> Dict.update 6 (always (((Dict.get 7 fishes |> Maybe.withDefault 0) + (Dict.get 0 fishes |> Maybe.withDefault 0)) |> Just))
-        |> Dict.update 7 (always (Dict.get 8 fishes |> Maybe.withDefault 0 |> Just))
-        |> Dict.update 8 (always (Dict.get 0 fishes |> Maybe.withDefault 0 |> Just))
+        |> Dict.insert 0 (old 1)
+        |> Dict.insert 1 (old 2)
+        |> Dict.insert 2 (old 3)
+        |> Dict.insert 3 (old 4)
+        |> Dict.insert 4 (old 5)
+        |> Dict.insert 5 (old 6)
+        |> Dict.insert 6 (old 7 + old 0)
+        |> Dict.insert 7 (old 8)
+        |> Dict.insert 8 (old 0)
