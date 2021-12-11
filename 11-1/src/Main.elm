49ac26a31eec988eb55fb26b4edf987b6bc9537a
Jeremy Hunt <huntingfrogs@hotmail.com>
2021-12-11 23:54:01 +1300

    Day 11 Part One

---
+++ 11-1/src/Main.elm
@@ -0,0 +1,153 @@
+module Main exposing (..)
+
+import Array exposing (Array)
+
+
+solve str =
+    { numFlashes = 0, data = parse str }
+        |> iterate 100 step
+        |> .numFlashes
+
+
+parse : String -> Array (Array Int)
+parse str =
+    str
+        |> String.lines
+        |> List.map (String.split "" >> List.filterMap String.toInt >> Array.fromList)
+        |> Array.fromList
+
+
+iterate : Int -> (a -> a) -> a -> a
+iterate times f a =
+    if times <= 0 then
+        a
+
+    else
+        iterate (times - 1) f (f a)
+
+
+step : { numFlashes : Int, data : Array (Array Int) } -> { numFlashes : Int, data : Array (Array Int) }
+step { numFlashes, data } =
+    let
+        octopi =
+            flash
+                { flashed = []
+                , octopi =
+                    data
+                        |> map ((+) 1)
+                        |> map (\energy -> { flashed = False, energy = energy })
+                }
+    in
+    { numFlashes = numFlashes + List.length octopi.flashed
+    , data = octopi.octopi |> map .energy
+    }
+
+
+
+-- Octopus
+
+
+type alias Octopus =
+    { flashed : Bool, energy : Int }
+
+
+shouldFlash : Octopus -> Bool
+shouldFlash { flashed, energy } =
+    not flashed && energy > 9
+
+
+flash : { flashed : List (Point Octopus), octopi : Array (Array Octopus) } -> { flashed : List (Point Octopus), octopi : Array (Array Octopus) }
+flash { flashed, octopi } =
+    let
+        newFlashers : List (Point Octopus)
+        newFlashers =
+            octopi
+                |> indexedMap (\x y -> Point ( x, y ))
+                |> elements
+                |> List.filter (.value >> shouldFlash)
+    in
+    if newFlashers == [] then
+        { flashed = flashed
+        , octopi = octopi |> resetFlashers flashed
+        }
+
+    else
+        flash
+            { flashed = flashed ++ newFlashers
+            , octopi =
+                octopi
+                    |> markFlashers newFlashers
+                    |> flashNeighbours newFlashers
+            }
+
+
+markFlashers : List (Point Octopus) -> Array (Array Octopus) -> Array (Array Octopus)
+markFlashers flashers octopi =
+    List.foldl (\{ pos } -> update pos (\octopus -> { octopus | flashed = True })) octopi flashers
+
+
+flashNeighbours : List (Point Octopus) -> Array (Array Octopus) -> Array (Array Octopus)
+flashNeighbours flashers octopi =
+    flashers
+        |> List.concatMap (\{ pos } -> neighbours octopi pos)
+        |> List.foldl (\{ pos } -> update pos (\octopus -> { octopus | energy = octopus.energy + 1 })) octopi
+
+
+resetFlashers : List (Point Octopus) -> Array (Array Octopus) -> Array (Array Octopus)
+resetFlashers flashers octopi =
+    List.foldl (\{ pos } -> update pos (\octopus -> { octopus | energy = 0 })) octopi flashers
+
+
+
+-- Array of Array
+
+
+foldl : (a -> b -> b) -> b -> Array (Array a) -> b
+foldl f =
+    Array.foldl (\row acc -> Array.foldl f acc row)
+
+
+elements : Array (Array a) -> List a
+elements =
+    foldl (::) []
+
+
+map : (a -> b) -> Array (Array a) -> Array (Array b)
+map f =
+    Array.map (Array.map f)
+
+
+update : ( Int, Int ) -> (a -> a) -> Array (Array a) -> Array (Array a)
+update ( x, y ) f xs =
+    case Array.get y xs of
+        Just row ->
+            case Array.get x row of
+                Just a ->
+                    xs |> Array.set y (row |> Array.set x (f a))
+
+                Nothing ->
+                    xs
+
+        Nothing ->
+            xs
+
+
+indexedMap : (Int -> Int -> a -> b) -> Array (Array a) -> Array (Array b)
+indexedMap f =
+    Array.indexedMap (\y row -> Array.indexedMap (\x a -> f x y a) row)
+
+
+type alias Point a =
+    { pos : ( Int, Int ), value : a }
+
+
+neighbours : Array (Array a) -> ( Int, Int ) -> List (Point a)
+neighbours array ( x, y ) =
+    List.filterMap
+        (\( dx, dy ) ->
+            array
+                |> Array.get (y + dy)
+                |> Maybe.andThen (Array.get (x + dx))
+                |> Maybe.map (Point ( x + dx, y + dy ))
+        )
+        [ ( -1, -1 ), ( 0, -1 ), ( 1, -1 ), ( -1, 0 ), ( 1, 0 ), ( -1, 1 ), ( 0, 1 ), ( 1, 1 ) ]
