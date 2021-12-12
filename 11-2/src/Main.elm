4c524b8cd4db1d02fef9d5f549c4b63736b21cd9
Jeremy Hunt <huntingfrogs@hotmail.com>
2021-12-12 16:31:04 +1300

    Simplify 11-2 further

--- 11-2/src/Main.elm
+++ 11-2/src/Main.elm
@@ -20,19 +20,13 @@ synchronize : Int -> Array (Array Int) -> Int
 synchronize iterations data =
     let
         result =
-            flash
-                { flashing = []
-                , octopi =
-                    data
-                        |> map ((+) 1)
-                        |> map (\energy -> { flashing = False, energy = energy })
-                }
+            map (Octopus False >> energize) data |> flash
     in
-    if all .flashing result.octopi then
+    if all .flashing result then
         iterations + 1
 
     else
-        synchronize (iterations + 1) (map .energy result.octopi)
+        synchronize (iterations + 1) (map .energy result)
 
 
 
@@ -43,54 +37,62 @@ type alias Octopus =
     { flashing : Bool, energy : Int }
 
 
+energize : Octopus -> Octopus
+energize octopus =
+    { octopus | energy = octopus.energy + 1 }
+
+
+reset : Octopus -> Octopus
+reset octopus =
+    { octopus | energy = 0 }
+
+
+startFlashing : Octopus -> Octopus
+startFlashing octopus =
+    { octopus | flashing = True }
+
+
 shouldFlash : Octopus -> Bool
 shouldFlash { flashing, energy } =
     not flashing && energy > 9
 
 
-flash :
-    { flashing : List (Point Octopus), octopi : Array (Array Octopus) }
-    -> { flashing : List (Point Octopus), octopi : Array (Array Octopus) }
-flash { flashing, octopi } =
+flash : Array (Array Octopus) -> Array (Array Octopus)
+flash octopi =
     let
+        points =
+            (indexedMap Point >> elements) octopi
+
         startedFlashing =
-            octopi
-                |> indexedMap (\x y -> Point ( x, y ))
-                |> elements
-                |> List.filter (.value >> shouldFlash)
+            List.filter (.value >> shouldFlash) points
     in
     if not <| List.isEmpty startedFlashing then
-        { flashing = startedFlashing ++ flashing
-        , octopi =
-            octopi
-                |> markFlashing startedFlashing
-                |> energizeNeighboursOf startedFlashing
-        }
+        octopi
+            |> markFlashing startedFlashing
+            |> energizeNeighboursOf startedFlashing
             |> flash
 
     else
-        { flashing = flashing
-        , octopi = octopi |> resetFlashing flashing
-        }
+        octopi |> resetFlashing (List.filter (.value >> .flashing) points)
 
 
 markFlashing : List (Point Octopus) -> Array (Array Octopus) -> Array (Array Octopus)
 markFlashing xs octopi =
-    List.foldl (\{ pos } -> update pos (\octopus -> { octopus | flashing = True }))
+    List.foldl (\{ pos } -> update pos startFlashing)
         octopi
         xs
 
 
 energizeNeighboursOf : List (Point Octopus) -> Array (Array Octopus) -> Array (Array Octopus)
 energizeNeighboursOf xs octopi =
-    List.foldl (\{ pos } -> update pos (\octopus -> { octopus | energy = octopus.energy + 1 }))
+    List.foldl (\{ pos } -> update pos energize)
         octopi
         (List.concatMap (\{ pos } -> neighbours octopi pos) xs)
 
 
 resetFlashing : List (Point Octopus) -> Array (Array Octopus) -> Array (Array Octopus)
 resetFlashing xs octopi =
-    List.foldl (\{ pos } -> update pos (\octopus -> { octopus | energy = 0 }))
+    List.foldl (\{ pos } -> update pos reset)
         octopi
         xs
 
@@ -134,9 +136,9 @@ update ( x, y ) f xs =
             xs
 
 
-indexedMap : (Int -> Int -> a -> b) -> Array (Array a) -> Array (Array b)
+indexedMap : (( Int, Int ) -> a -> b) -> Array (Array a) -> Array (Array b)
 indexedMap f =
-    Array.indexedMap (\y row -> Array.indexedMap (\x a -> f x y a) row)
+    Array.indexedMap (\y row -> Array.indexedMap (\x a -> f ( x, y ) a) row)
 
 
 type alias Point a =
