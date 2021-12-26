be04de190e1ce0345e2913d350211ed0b839788a
Jeremy Hunt <huntingfrogs@hotmail.com>
2021-12-26 20:47:46 +1300

    Tidy up 14-2

--- 14-2/src/Main.elm
+++ 14-2/src/Main.elm
@@ -19,11 +19,13 @@ solve str =
         |> orElseCrash
 
 
-parse : String -> ( List Char, Dict ( Char, Char ) Char )
+parse : String -> ( List Char, Ruleset )
 parse str =
     case String.lines str of
         template :: _ :: rules ->
-            ( String.toList template, List.map parseRule rules |> Dict.fromList )
+            ( String.toList template
+            , Dict.fromList (List.map parseRule rules)
+            )
 
         _ ->
             Debug.todo "Invalid input"
@@ -32,9 +34,9 @@ parse str =
 parseRule : String -> ( ( Char, Char ), Char )
 parseRule str =
     case String.split " -> " str of
-        pair :: insert :: [] ->
+        [ pair, insert ] ->
             case ( String.toList pair, String.toList insert ) of
-                ( a :: b :: [], c :: [] ) ->
+                ( [ a, b ], [ c ] ) ->
                     ( ( a, b ), c )
 
                 _ ->
@@ -44,7 +46,7 @@ parseRule str =
             Debug.todo "Invalid input"
 
 
-pairCountFromTemplate : List Char -> Dict ( Char, Char ) Int
+pairCountFromTemplate : List Char -> PairCounts
 pairCountFromTemplate template =
     List.map2 Tuple.pair
         template
@@ -52,7 +54,19 @@ pairCountFromTemplate template =
         |> Dict.Extra.frequencies
 
 
-updateFrequencies : Dict ( Char, Char ) Char -> ( Dict ( Char, Char ) Int, Dict Char Int ) -> ( Dict ( Char, Char ) Int, Dict Char Int )
+type alias Ruleset =
+    Dict ( Char, Char ) Char
+
+
+type alias PairCounts =
+    Dict ( Char, Char ) Int
+
+
+type alias LetterCounts =
+    Dict Char Int
+
+
+updateFrequencies : Ruleset -> (( PairCounts, LetterCounts ) -> ( PairCounts, LetterCounts ))
 updateFrequencies rules ( pairCounts, letterCounts ) =
     Dict.foldl
         (\( a, b ) count ( pairCounts_, letterCounts_ ) ->
@@ -60,10 +74,12 @@ updateFrequencies rules ( pairCounts, letterCounts ) =
                 inserted =
                     Dict.get ( a, b ) rules |> orElseCrash
             in
-            ( Dict.update ( a, b ) (Maybe.map (\latest -> latest - count) >> orElseCrash >> Just) pairCounts_
+            ( pairCounts_
+                |> Dict.update ( a, b ) (Maybe.map (\latest -> latest - count) >> orElseCrash >> Just)
                 |> Dict.update ( a, inserted ) (Maybe.withDefault 0 >> (+) count >> Just)
                 |> Dict.update ( inserted, b ) (Maybe.withDefault 0 >> (+) count >> Just)
-            , Dict.update inserted (Maybe.withDefault 0 >> (+) count >> Just) letterCounts_
+            , letterCounts_
+                |> Dict.update inserted (Maybe.withDefault 0 >> (+) count >> Just)
             )
         )
         ( pairCounts, letterCounts )
