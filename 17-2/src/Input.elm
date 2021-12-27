b5c95fe114920a2567769b93dbfffc8f65f1d8db
Jeremy Hunt <huntingfrogs@hotmail.com>
2021-12-27 18:36:33 +1300

    Day 17-2

---
+++ 17-2/src/Input.elm
@@ -0,0 +1,11 @@
+module Input exposing (..)
+
+
+testInput : String
+testInput =
+    """target area: x=20..30, y=-10..-5"""
+
+
+input : String
+input =
+    """target area: x=211..232, y=-124..-69"""
