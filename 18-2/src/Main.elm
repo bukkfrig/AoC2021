16867b78b4708611344325cf5fce68b8f2b58ae5
Jeremy Hunt <huntingfrogs@hotmail.com>
2022-01-08 14:18:07 +1300

    Tidy up new 18-2

--- 18-2/src/Main.elm
+++ 18-2/src/Main.elm
@@ -1,33 +1,55 @@
 module Main exposing (solve)
 
 import Array exposing (Array)
-import Parser exposing ((|.), (|=), Parser)
+import Parser exposing ((|=), Parser)
 
 
 solve str =
     let
-        snails : List (Array Token)
         snails =
-            List.map (okOrCrash "Couldn't parse a snail number?") (parse str)
+            parse str
+                |> List.map (okOrCrash "Couldn't parse a snail number?")
     in
     List.foldl
-        (\first best ->
-            List.maximum (best :: List.map (add first >> magnitude) snails ++ List.map (\second -> add second first |> magnitude) snails)
-                |> orElseCrash "No maximum?"
-                |> Debug.log "Maximum so far: "
+        (\this max ->
+            List.concat
+                [ List.map (\other -> add this other |> magnitude) snails
+                , List.map (\other -> add other this |> magnitude) snails
+                , List.singleton max
+                ]
+                |> (List.maximum >> orElseCrash "No maximum?")
+                |> Debug.log "Maximum so far"
         )
         0
         snails
 
 
-parse : String -> List (Result (List Parser.DeadEnd) (Array Token))
+type alias Tokens =
+    Array Token
+
+
+type alias DeadEnds =
+    List Parser.DeadEnd
+
+
+parse : String -> List (Result DeadEnds Tokens)
 parse str =
     String.lines str
-        |> List.map (Parser.run parseTokens)
+        |> List.map (Parser.run tokenParser)
 
 
-parseTokens : Parser (Array Token)
-parseTokens =
+
+-- Tokens
+
+
+type Token
+    = Open
+    | Close
+    | Regular Int
+
+
+tokenParser : Parser Tokens
+tokenParser =
     Parser.loop []
         (\tokens ->
             Parser.oneOf
@@ -45,165 +67,6 @@ parseTokens =
         )
 
 
-
--- Snail number logic
-
-
-type alias Tokens =
-    Array Token
-
-
-type Token
-    = Open
-    | Close
-    | Regular Int
-
-
-magnitude : Tokens -> Int
-magnitude sn =
-    magnitudeHelp 0 [] sn
-
-
-magnitudeHelp index stack tokens =
-    case Array.get index tokens of
-        Just (Regular n2) ->
-            case stack of
-                [] ->
-                    n2
-
-                ( Nothing, Nothing ) :: xs ->
-                    magnitudeHelp (index + 1) (( Just n2, Nothing ) :: xs) tokens
-
-                ( Just n1, Nothing ) :: xs ->
-                    magnitudeHelp (index + 1) (( Just n1, Just n2 ) :: xs) tokens
-
-                _ ->
-                    Debug.todo "This stack frame already complete?"
-
-        Just Open ->
-            magnitudeHelp (index + 1) (( Nothing, Nothing ) :: stack) tokens
-
-        Just Close ->
-            case stack of
-                [] ->
-                    Debug.todo "Empty stack?"
-
-                ( Just n1, Just n2 ) :: xs ->
-                    let
-                        mag =
-                            3 * n1 + 2 * n2
-                    in
-                    case xs of
-                        [] ->
-                            mag
-
-                        ( Nothing, Nothing ) :: rest ->
-                            magnitudeHelp (index + 1) (( Just mag, Nothing ) :: rest) tokens
-
-                        ( Just m1, Nothing ) :: rest ->
-                            magnitudeHelp (index + 1) (( Just m1, Just mag ) :: rest) tokens
-
-                        _ ->
-                            Debug.todo "Next stack frame already complete?"
-
-                _ ->
-                    Debug.todo "Incomplete stack frame?"
-
-        Nothing ->
-            Debug.todo "Stack didn't empty before end of tokens?"
-
-
-add : Tokens -> Tokens -> Tokens
-add s1 s2 =
-    reduce
-        (Array.empty
-            |> Array.push Open
-            |> (\tokens -> Array.append tokens s1)
-            |> (\tokens -> Array.append tokens s2)
-            |> Array.push Close
-        )
-
-
-reduce : Tokens -> Tokens
-reduce sn =
-    case explode sn of
-        Exploded tokens ->
-            reduce tokens
-
-        NotExploded ->
-            case split sn of
-                Split tokens ->
-                    reduce tokens
-
-                NotSplit ->
-                    sn
-
-
-type ExplodeResult
-    = Exploded Tokens
-    | NotExploded
-
-
-type SplitResult
-    = Split Tokens
-    | NotSplit
-
-
-deletePairStartingAt : Int -> Tokens -> Tokens
-deletePairStartingAt n tokens =
-    tokens
-        |> Array.slice 0 (n - 1)
-        |> Array.push (Regular 0)
-        |> (\t -> Array.append t (Array.slice (n + 3) (Array.length tokens) tokens))
-
-
-explode : Tokens -> ExplodeResult
-explode sn =
-    explodeHelp 0 0 Nothing sn
-
-
-explodeHelp : Int -> Int -> Maybe ( Int, Int ) -> Tokens -> ExplodeResult
-explodeHelp index depth lastRegular sn =
-    if depth > 4 then
-        case ( Array.get index sn, Array.get (index + 1) sn ) of
-            ( Just (Regular n1), Just (Regular n2) ) ->
-                (case ( lastRegular, arrayFind isRegular (Array.slice (index + 3) (Array.length sn) sn) ) of
-                    ( Just ( li, l1 ), Just ( ri, Regular r1 ) ) ->
-                        sn |> Array.set li (Regular (l1 + n1)) |> Array.set (ri + index + 3) (Regular (r1 + n2))
-
-                    ( Just ( li, l1 ), Nothing ) ->
-                        Array.set li (Regular (l1 + n1)) sn
-
-                    ( Nothing, Just ( ri, Regular r1 ) ) ->
-                        Array.set (ri + index + 3) (Regular (r1 + n2)) sn
-
-                    ( Nothing, Nothing ) ->
-                        sn
-
-                    _ ->
-                        Debug.todo "Found something that wasn't regular?"
-                )
-                    |> deletePairStartingAt index
-                    |> Exploded
-
-            _ ->
-                Debug.todo ("Not a pair at depth 4 with index " ++ Debug.toString index)
-
-    else
-        case Array.get index sn of
-            Nothing ->
-                NotExploded
-
-            Just Open ->
-                explodeHelp (index + 1) (depth + 1) lastRegular sn
-
-            Just Close ->
-                explodeHelp (index + 1) (depth - 1) lastRegular sn
-
-            Just (Regular n1) ->
-                explodeHelp (index + 1) depth (Just ( index, n1 )) sn
-
-
 isRegular : Token -> Bool
 isRegular token =
     case token of
@@ -214,72 +77,298 @@ isRegular token =
             False
 
 
-arrayFind : (a -> Bool) -> Array a -> Maybe ( Int, a )
-arrayFind f xs =
-    arrayFindHelp 0 f xs
+
+-- Snail number logic
 
 
-arrayFindHelp : Int -> (a -> Bool) -> Array a -> Maybe ( Int, a )
-arrayFindHelp n f xs =
-    case Array.get n xs of
-        Just x ->
-            if f x then
-                Just ( n, x )
+add : Tokens -> Tokens -> Tokens
+add s1 s2 =
+    reduce
+        (Array.empty
+            |> Array.push Open
+            |> (\stream -> Array.append stream s1)
+            |> (\stream -> Array.append stream s2)
+            |> Array.push Close
+        )
+
+
+reduce : Tokens -> Tokens
+reduce sn =
+    case explode sn of
+        Modified tokens ->
+            reduce tokens
+
+        NotModified ->
+            case split sn of
+                Modified tokens ->
+                    reduce tokens
+
+                NotModified ->
+                    sn
+
+
+{-| Replace something like:
+
+        [ ..., Open, Regular 1, Regular 2, Close, ... ]
+
+    with
+
+        [ ..., Regular 0, ... ]
+
+-}
+deletePairStartingAt : Int -> Tokens -> Tokens
+deletePairStartingAt n tokens =
+    let
+        length =
+            Array.length tokens
+
+        ( left, right ) =
+            ( Array.slice 0 (n - 1) tokens
+            , Array.slice (n + 3) length tokens
+            )
+    in
+    Array.empty
+        |> (\stream -> Array.append stream left)
+        |> Array.push (Regular 0)
+        |> (\stream -> Array.append stream right)
+
+
+explode : Tokens -> Modification Tokens
+explode tokens =
+    let
+        go depth index leftNeighbour =
+            if depth > 4 then
+                Modified <|
+                    case
+                        ( tokens |> Array.get index
+                        , tokens |> Array.get (index + 1)
+                        )
+                    of
+                        ( Just (Regular n1), Just (Regular n2) ) ->
+                            let
+                                rightNeighbour =
+                                    let
+                                        length =
+                                            tokens |> Array.length
+
+                                        startOfUnvisited =
+                                            index + 3
+
+                                        unvisited =
+                                            tokens |> Array.slice startOfUnvisited length
+                                    in
+                                    case arrayFind isRegular unvisited of
+                                        Just ( ix, Regular n ) ->
+                                            Just ( startOfUnvisited + ix, n )
+
+                                        _ ->
+                                            Nothing
+
+                                updateNeighbours =
+                                    case ( leftNeighbour, rightNeighbour ) of
+                                        ( Nothing, Nothing ) ->
+                                            identity
+
+                                        ( Just ( leftIndex, l1 ), Nothing ) ->
+                                            Array.set leftIndex (Regular (l1 + n1))
+
+                                        ( Nothing, Just ( rightIndex, r1 ) ) ->
+                                            Array.set rightIndex (Regular (n2 + r1))
+
+                                        ( Just ( leftIndex, l1 ), Just ( rightIndex, r1 ) ) ->
+                                            identity
+                                                >> Array.set leftIndex (Regular (l1 + n1))
+                                                >> Array.set rightIndex (Regular (n2 + r1))
+                            in
+                            tokens
+                                |> updateNeighbours
+                                |> deletePairStartingAt index
+
+                        _ ->
+                            Debug.todo ("Not a pair at depth 4 with index " ++ Debug.toString index)
 
             else
-                arrayFindHelp (n + 1) f xs
+                case tokens |> Array.get index of
+                    Nothing ->
+                        NotModified
 
-        Nothing ->
-            Nothing
+                    Just Open ->
+                        go
+                            (depth + 1)
+                            (index + 1)
+                            leftNeighbour
+
+                    Just Close ->
+                        go
+                            (depth - 1)
+                            (index + 1)
+                            leftNeighbour
+
+                    Just (Regular n1) ->
+                        go
+                            depth
+                            (index + 1)
+                            (Just ( index, n1 ))
+    in
+    go
+        0
+        0
+        Nothing
 
 
-split : Tokens -> SplitResult
-split sn =
-    splitHelp 0 sn
+split : Tokens -> Modification Tokens
+split tokens =
+    let
+        go index =
+            case Array.get index tokens of
+                Just (Regular n) ->
+                    if n >= 10 then
+                        Modified <|
+                            let
+                                length =
+                                    tokens |> Array.length
+
+                                ( left, right ) =
+                                    ( tokens |> Array.slice 0 index
+                                    , tokens |> Array.slice (index + 1) length
+                                    )
+                            in
+                            Array.empty
+                                |> (\stream -> Array.append stream left)
+                                |> Array.push Open
+                                |> Array.push (Regular (n // 2))
+                                |> Array.push (Regular (ceiling (toFloat n / 2)))
+                                |> Array.push Close
+                                |> (\stream -> Array.append stream right)
+
+                    else
+                        go
+                            (index + 1)
+
+                Nothing ->
+                    NotModified
+
+                Just _ ->
+                    go
+                        (index + 1)
+    in
+    go
+        0
 
 
-splitHelp : Int -> Tokens -> SplitResult
-splitHelp index sn =
-    case Array.get index sn of
-        Nothing ->
-            NotSplit
+magnitude : Tokens -> Int
+magnitude tokens =
+    let
+        go index stack =
+            case Array.get index tokens of
+                Just Open ->
+                    -- Push an empty stack frame
+                    go
+                        (index + 1)
+                        (( Nothing, Nothing ) :: stack)
 
-        Just (Regular n) ->
-            if n < 10 then
-                splitHelp (index + 1) sn
+                Just (Regular n) ->
+                    -- Fill in the frame at the top of the stack. Does it need it's first or second number?
+                    case stack of
+                        [] ->
+                            n
 
-            else
-                sn
-                    |> Array.slice 0 index
-                    |> Array.push Open
-                    |> Array.push (Regular (n // 2))
-                    |> Array.push (Regular (ceiling (toFloat n / 2)))
-                    |> Array.push Close
-                    |> (\t -> Array.append t (Array.slice (index + 1) (Array.length sn) sn))
-                    |> Split
+                        ( Nothing, Nothing ) :: xs ->
+                            go
+                                (index + 1)
+                                (( Just n, Nothing ) :: xs)
 
-        Just _ ->
-            splitHelp (index + 1) sn
+                        ( Just n1, Nothing ) :: xs ->
+                            go
+                                (index + 1)
+                                (( Just n1, Just n ) :: xs)
+
+                        _ ->
+                            Debug.todo "This stack frame already complete?"
+
+                Just Close ->
+                    -- Pop the top stack frame , turn it into a magnitude, then put it into the next frame of the stack.
+                    case stack of
+                        ( Just n1, Just n2 ) :: xs ->
+                            let
+                                mag =
+                                    3 * n1 + 2 * n2
+                            in
+                            case xs of
+                                [] ->
+                                    mag
+
+                                ( Nothing, Nothing ) :: rest ->
+                                    go
+                                        (index + 1)
+                                        (( Just mag, Nothing ) :: rest)
+
+                                ( Just m1, Nothing ) :: rest ->
+                                    go
+                                        (index + 1)
+                                        (( Just m1, Just mag ) :: rest)
+
+                                _ ->
+                                    Debug.todo "Next stack frame already complete?"
+
+                        [] ->
+                            Debug.todo "Empty stack?"
+
+                        _ ->
+                            Debug.todo "Incomplete stack frame?"
+
+                Nothing ->
+                    Debug.todo "Stack didn't empty before end of tokens?"
+    in
+    go
+        0
+        []
 
 
 
 -- Basics
 
 
-orElseCrash s m =
-    case m of
+type Modification a
+    = Modified a
+    | NotModified
+
+
+okOrCrash : String -> Result error a -> a
+okOrCrash reason result =
+    case result of
+        Ok a ->
+            a
+
+        Err error ->
+            Debug.todo (reason ++ "\n" ++ Debug.toString error)
+
+
+orElseCrash : String -> Maybe a -> a
+orElseCrash reason maybe =
+    case maybe of
         Just a ->
             a
 
         Nothing ->
-            Debug.todo s
+            Debug.todo reason
 
 
-okOrCrash : String -> Result e a -> a
-okOrCrash s r =
-    case r of
-        Ok a ->
-            a
+arrayFind : (a -> Bool) -> Array a -> Maybe ( Int, a )
+arrayFind f xs =
+    let
+        go n =
+            case Array.get n xs of
+                Just x ->
+                    if f x then
+                        Just ( n, x )
 
-        Err e ->
-            Debug.todo (s ++ "\n" ++ Debug.toString e)
+                    else
+                        go
+                            (n + 1)
+
+                Nothing ->
+                    Nothing
+    in
+    go
+        0
